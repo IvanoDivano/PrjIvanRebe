01c3527a7a1244cae37e1db687873330f13a2de0
IvanoDivano <tengaivan.taggia@gmail.com>
2023-04-26 18:33:16 +0200

    Registration forms now add user to db

--- scripts/action.php
+++ scripts/action.php
@@ -1,12 +1,30 @@
 <?php
+
+    $host = 'localhost';
+    $user = 'S4797564';
+    $dbpw = 'Un_Etto_Di_Cotto';
+    $database = 'S4797564';
+
+
     $firstname = $_POST['firstname'];
     $lastname = $_POST['lastname'];
     $email = $_POST['email'];
     $pass = $_POST['pass'];
 
+    $hashedPass = password_hash($pass, PASSWORD_ARGON2I);
 
-    
-    $file = fopen('data.txt', 'a');
-    fwrite($file, "$firstname, $lastname, $email, $pass\n");
-    fclose($file);
+    $dbConnection = new mysqli($hostname, $username, $dbpw,$db);
+
+    if ($dbConnection->connect_error){die('Connection to the database failed: ' . $dbConnection->connect_error);}
+
+    $query = "INSERT INTO users (firstname, lastname, email, password) VALUES ('$firstname', '$lastname', '$email', '$hashedPass')";
+
+    if ($dbConnection->query($query) === TRUE){
+        echo "Added new user!";
+    }
+    else{
+        echo "Error adding user: " . $dbConnection->error; 
+    }
+
+    $dbConnection->close();
 ?>
