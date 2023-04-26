ea15e78554b88f0a8c2230fa99c54ba5a5e682aa
RebeccaSanua <RebeccaSanua@outlook.it>
2023-04-26 20:00:50 +0200

    modifica login

--- html/areaClienti.php
+++ html/areaClienti.php
@@ -17,25 +17,28 @@
         <aside>
             <h1>Accedi</h1>
             <h2>alla nostra area clienti</h2>
+            <p>Potrai gestire facilmente <b>il tuo account</b> e le tue prenotazioni di car sharing. <br>Potrai visualizzare lo storico delle tue <b>prenotazioni</b>, <br> modificare le tue <b>informazioni personali</b> e gestire i tuoi metodi di pagamento. <br> Inoltre, avrai accesso a <b>offerte esclusive</b> e promozioni riservate ai nostri clienti fedeli. <br>Scopri tutto ciò che l’area clienti ha da offrirti e sfrutta al massimo i vantaggi del car sharing!</p>
             <h3>Non sei ancora cliente GoGreen?</h3>
             <a href="Registrazone.php"> Registrati </a>
         </aside>
         <form>
+            <h1>Inserisci i tuoi dati</h1>
             <input type="email" id="email" name="email" placeholder="Inserisci email" required oninvalid="this.setCustomValidity('Il campo è obbligatorio')" required pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.(com|it|org|net|edu|gov|mil|biz|info|io|me|tv|co)$">
             <input type="password" id="pass" name="pass" placeholder="Inserisci password" required pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+,-.;'/?[\]{}|`~=:]).{8,}$" title="La password deve contenere almeno un numero, una lettera maiuscola, una lettera minuscola, un carattere speciale e minimo 8 caratteri">
-            <p id="show-password"><input type="checkbox" onclick="myFunction()"> Mostra password</p>
-            <p><a id="linkInvio" href=""><input id="invia" type="submit" value="Invia"></a></p>
-            <script>
-                function myFunction() {
-                    var x = document.getElementById("pass");
-                    if ( x.type === "password") {x.type = "text";} 
-                    else {x.type = "password";}
-                }
-                document.getElementById("email").addEventListener("input", function() {
-                    this.setCustomValidity("");
-                });
-            </script>
+            <input type="checkbox" onclick="myFunction()"> <p>Mostra password</p>
+            <button>Invia</button>
         </form>
+        <script>
+            function myFunction() {
+                            var x = document.getElementById("pass");
+                            if ( x.type === "password") {x.type = "text";} 
+                            else {x.type = "password";}
+            }
+
+            document.getElementById("email").addEventListener("input", function() {
+                            this.setCustomValidity("");
+            });
+        </script>
     </section>
     <?php include './components/footer.html'; ?>
 </body>
