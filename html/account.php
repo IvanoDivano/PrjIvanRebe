6472d452a7919de21a0166c9131a7aeff36541e6
RebeccaSanua <RebeccaSanua@outlook.it>
2023-05-02 17:24:39 +0200

    account

---
+++ html/account.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>GoGreen</title>
+
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Lora">
+    <link rel="stylesheet" href="../css/headerFooter.css">
+    <link rel="stylesheet" href="../css/index.css">
+    <link rel="icon" href="../res/GoGreen-vuoto.png">
+</head>
+<body>
+    <?php include './components/header.html'; ?>
+    <?php include './components/footer.html'; ?>
+</body>
+</html>
